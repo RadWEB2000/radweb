708e44569ae2d5fa2d1579745a8341342a5bc4a8
Radosław Adamczyk <radoslaw.adamczyk2000@gmail.com>
2024-07-12 21:04:08 +0200

    crm wenb

---
+++ web/app/page.tsx
@@ -0,0 +1,9 @@
+export default function HomePage(){
+  return (
+    <>
+      <main>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic vero inventore a est recusandae dolor perferendis magni voluptatem illum architecto perspiciatis id omnis, doloremque nobis ipsam quae cupiditate! Cumque, perferendis.
+      </main>
+    </>
+  )
+}
