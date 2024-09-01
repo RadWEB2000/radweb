560703670f72ffea42344f32e2060a101bc59eb9
RadWEB2000 <radoslaw.adamczyk2000@gmail.com>
2024-09-01 17:53:05 +0200

    finish testimonials slider

--- web-idk/components/Views/HomeView/Testimonials/Testimonials.tsx
+++ web-idk/components/Views/HomeView/Testimonials/Testimonials.tsx
@@ -1,11 +1,55 @@
+"use client"
 import css from "v-home/Testimonials/Testimonials.module.scss"
 import {tTestimonials} from "v-home/Testimonials/Testimonials.models"
 import { TestimonialCard as Card} from "cards/index"
 import { FaChevronLeft as Left,FaChevronRight as Right } from "react-icons/fa";
+import Slider from "react-slick";
 
-
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function Testimonials(props:tTestimonials){
+    const settings = {
+        dots: true,
+        infinite: true,
+        className:css.cards,
+        speed: 350,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay:true,
+        // centerMode: true,
+        autoplaySpeed:2250,
+        pauseOnHover:true,
+        responsive: [
+            {
+                breakpoint:1550,
+                settings: {
+                    arrows:true,
+                    slidesToShow: 4,
+                }
+            },
+            {
+                breakpoint:1050,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint:780,
+                settings: {
+                    arrows:false,
+                    slidesToShow: 2,
+                }
+            }, 
+            {
+                breakpoint:550,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+            
+        ]
+    }
     return (
         <div
             className={css.wrapper}
@@ -25,7 +69,7 @@ export default function Testimonials(props:tTestimonials){
             {
                 props.cards &&
                 <>
-                    <ul
+                    {/* <ul
                         className={css.cards}
                     >
                         {
@@ -38,21 +82,23 @@ export default function Testimonials(props:tTestimonials){
                                 )
                             })
                         }
-                    </ul>
-                    <div 
-                        className={css.buttons}
+                    </ul> */}
+                    <Slider
+                        {...settings}
+
                     >
-                        <button
-                            className={css.button}
-                        >
-                            <Left/>
-                        </button>
-                        <button
-                            className={css.button}
-                        >
-                            <Right/>
-                        </button>
-                    </div>
+                        {
+                            props.cards.map((item, index) => {
+                                return (
+                                    <Card
+                                        {...item}
+                                        key={index}
+                                    />
+                                )
+                            })
+                        }
+                    </Slider>
+                  
                 </>
             }
             
