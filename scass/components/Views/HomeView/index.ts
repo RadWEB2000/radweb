821059c1372150e1dc4ae7b133c32b32b30bf758
Radosław Adamczyk <radoslaw.adamczyk2000@gmail.com>
2024-09-25 20:57:42 +0200

    services section

--- scass/components/Views/HomeView/index.ts
+++ scass/components/Views/HomeView/index.ts
@@ -3,3 +3,4 @@ export {default as Blog} from "v-home/Blog/Blog";
 export {default as Hero} from "v-home/Hero/Hero";
 export {default as Offer} from "v-home/Offer/Offer";
 export {default as Projects} from "v-home/Projects/Projects";
+export {default as Services} from "v-home/Services/Services"
